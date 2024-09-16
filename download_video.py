605c8520a92544a01bbd969bc37d12dd18508a9a
Phil Liao <cheweiphil@gmail.com>
2024-09-16 08:47:08 +0800

    Reformatted files

--- download_video.py
+++ download_video.py
@@ -1,5 +1,4 @@
-import yt_dlp as _youtube_dl
-import youtube_dl
+import yt_dlp as youtube_dl
 def download_video(url):
   ydl_opts = {'outtmpl': 'video_files/%(title)s.%(ext)s',
               'cookiefile': 'cookies.firefox-private.txt',
@@ -7,7 +6,7 @@ def download_video(url):
                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 },
               }
-  with _youtube_dl.YoutubeDL(ydl_opts) as ydl:
+  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
       ydl.download([url])
       result = ydl.extract_info("{}".format(url))
       title = result.get("title", None)
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import streamlit as st
-import yt_dlp
+import yt_dlp as youtube_dl
 import os
 
-import download_video
 
-
+def download_video(url):
+  ydl_opts = {'outtmpl': 'video_files/%(title)s.%(ext)s',
+              'cookiefile': 'cookies.firefox-private.txt',
+              'http_headers': {
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                },
+              }
+  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+      ydl.download([url])
+      result = ydl.extract_info("{}".format(url))
+      title = result.get("title", None)
+      return title
 
 title = st.title("The video downloader created with YouTube DL")
 divider_1 = st.divider()
@@ -15,7 +25,7 @@ download_button = st.button("Download", type="primary", disabled=display_1)
 
 
 if download_button:
-    location = download_video.download_video(url)
+    location = download_video(url)
     display_1 = True
     with open(f"video_files/{location}.mp4", "rb") as file:
         download_file = st.download_button(

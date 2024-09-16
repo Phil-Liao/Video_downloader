49fda54bec6feb15dd47cc60af7f09e7d4c4d817
Phil Liao <cheweiphil@gmail.com>
2024-09-16 08:43:57 +0800

    Test

--- download_video.py
+++ download_video.py
@@ -1,7 +1,12 @@
 import yt_dlp as _youtube_dl
 import youtube_dl
 def download_video(url):
-  ydl_opts = {'outtmpl': 'video_files/%(title)s.%(ext)s'}
+  ydl_opts = {'outtmpl': 'video_files/%(title)s.%(ext)s',
+              'cookiefile': 'cookies.firefox-private.txt',
+              'http_headers': {
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                },
+              }
   with _youtube_dl.YoutubeDL(ydl_opts) as ydl:
       ydl.download([url])
       result = ydl.extract_info("{}".format(url))
